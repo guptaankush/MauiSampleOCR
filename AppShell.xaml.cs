cc788e97c050e53d0f22a3238f853bd6e38f3de7
Ankush <agupta@medullus.com>
2024-09-08 17:57:17 +0530

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUISampleOCR
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Plugin.Maui.OCR;
+
+namespace MAUISampleOCR
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly IOcrService _ocrService;
+
+        public MainPage(IOcrService ocrService)
+        {
+            _ocrService = ocrService ?? OcrPlugin.Default;
+            InitializeComponent();
+        }
+
+        //protected async override void OnAppearing()
+        //{
+        //    base.OnAppearing();
+
+        //    await OcrPlugin.Default.InitAsync();
+        //}
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var pickResult = await MediaPicker.Default.PickPhotoAsync();
+                await CommonMethod(pickResult);
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Exception", ex.Message, "Okay");
+            }
+        }
+
+        private async void CounterBtn2_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var pickResult = await MediaPicker.Default.CapturePhotoAsync();
+                await CommonMethod(pickResult);
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Exception", ex.Message, "Okay");
+            }
+        }
+
+        private async Task CommonMethod(FileResult? pickResult)
+        {
+            if (pickResult != null)
+            {
+                using var imageAsStream = await pickResult.OpenReadAsync();
+                var imageAsByteArr = new byte[imageAsStream.Length];
+                await imageAsStream.ReadAsync(imageAsByteArr);
+
+                var result = await _ocrService.RecognizeTextAsync(imageAsByteArr, true);
+
+                if (result.Success)
+                    await DisplayAlert("OCR Result", result.AllText, "Okay");
+                else
+                    await DisplayAlert("No success", "No OCR possible", "OK");
+            }
+        }
+    }
+
+}
